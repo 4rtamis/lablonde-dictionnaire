02f84a4864222090d34a9034b333481803908cfe
Artamis <artamis@hackademint.org>
2023-11-11 23:01:41 +0100

    Update index.astro. Add dark / light mode switch.

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "preact/hooks";
+
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // or null;
+  }
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <button
+      class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+      onClick={toggleTheme}
+    >
+      {theme === "dark" ? "🌙" : "🌞"}
+      <span class="sr-only">Toggle Theme</span>
+    </button>
+  );
+}
