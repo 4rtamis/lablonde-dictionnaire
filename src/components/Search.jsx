120a50f47965e825df3225140626723911d300ed
Artamis <artamis@hackademint.org>
2023-11-22 11:36:02 +0100

    Add search bar keyboard navigation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,9 +14,13 @@ function Search({ searchList }) {
   // User's input
   const [query, setQuery] = useState("");
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [dropdownSelectPosition, setDropdownSelectPosition] = useState(null);
   let isMouseOut = false;
-
   const fuse = new Fuse(searchList, options);
+
+  function mod(n, m) {
+    return ((n % m) + m) % m;
+  }
 
   // Set a limit to the words: 5
   const words = fuse
@@ -33,12 +37,50 @@ function Search({ searchList }) {
   function handleOnFocusOut() {
     if (isMouseOut) {
       setIsDropdownVisible(false);
+      setDropdownSelectPosition(null);
       isMouseOut = false;
     }
   }
 
   function handleOnMouseLeave() {
     isMouseOut = true;
+  }
+
+  function handleListControls(event) {
+    switch (event.key) {
+      case "ArrowDown":
+        dropdownSelectPosition === null
+          ? setDropdownSelectPosition(0)
+          : setDropdownSelectPosition(
+              mod(dropdownSelectPosition + 1, words.length)
+            );
+        event.preventDefault();
+        break;
+      case "ArrowUp":
+        dropdownSelectPosition === null
+          ? setDropdownSelectPosition(words.length - 1)
+          : setDropdownSelectPosition(
+              mod(dropdownSelectPosition - 1, words.length)
+            );
+        event.preventDefault();
+        break;
+      case "Enter":
+        if (dropdownSelectPosition === null) {
+          window.location.href = `/${words[0].mot}`;
+        } else {
+          window.location.href = `/${words[dropdownSelectPosition].mot}`;
+        }
+        break;
+      case "Escape":
+        setIsDropdownVisible(false);
+        setDropdownSelectPosition(null);
+        isMouseOut = false;
+        event.preventDefault();
+        break;
+      default:
+        handleOnSearch(event);
+        return; // Quit when this doesn't handle the key event.
+    }
   }
 
   return (
@@ -67,6 +109,8 @@ function Search({ searchList }) {
         value={query}
         onInput={handleOnSearch}
         onFocus={handleOnSearch}
+        onClick={handleOnSearch}
+        onKeyDown={handleListControls}
         placeholder="Rechercher..."
         class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-yellow-500 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500 focus-visible:outline-none"
       />
@@ -79,11 +123,15 @@ function Search({ searchList }) {
             class="py-2 text-sm text-gray-700 dark:text-gray-200"
             aria-labelledby="dropdownDefaultButton"
           >
-            {words.map((word) => (
+            {words.map((word, i) => (
               <li>
                 <a
                   href={`/${word.mot}`}
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  className={
+                    dropdownSelectPosition == i
+                      ? "block px-4 py-2 dark:text-white bg-gray-300 dark:bg-gray-500"
+                      : "block px-4 py-2 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
+                  }
                 >
                   {word.mot}{" "}
                   <span class="text-yellow-600">
